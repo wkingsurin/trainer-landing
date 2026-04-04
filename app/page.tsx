cc24682b870f05807da8150523b8922f0128aab2
Daniil Surin <wkingsurin@gmail.com>
2026-04-04 19:28:46 +0500

    feat(home): render problems component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
+import Problem from "@/components/problems";
 
 export default function Home() {
 	return (
 		<main>
 			<Hero />
+			<Problem />
 		</main>
 	);
 }

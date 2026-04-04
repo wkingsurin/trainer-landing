2c504131679d8ccbea2a554914e31dfe1affedb0
Daniil Surin <wkingsurin@gmail.com>
2026-04-04 16:30:59 +0500

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
 	variable: "--font-inter",
@@ -21,7 +24,7 @@ export default function RootLayout({
 	return (
 		<html
 			lang="ru"
-			className={`${inter.variable} h-full antialiased`}
+			className={cn("h-full", "antialiased", inter.variable, "font-sans", geist.variable)}
 		>
 			<body className="min-h-full flex flex-col">{children}</body>
 		</html>

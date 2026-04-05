cc3977dd9db9fac51c2c0d44f74bbf26469aa574
Daniil Surin <wkingsurin@gmail.com>
2026-04-05 14:17:57 +0500

    feat(widgets): improve CardsBlock
    - Replaced BaseCard with Card
    - Added square flag to set aspect-ratio 1:1 to BaseCard
    - Corrected JSX for render icon or string

--- components/widgets/cards-block.tsx
+++ components/widgets/cards-block.tsx
@@ -1,9 +1,10 @@
-import Card from "../shared/card";
+import BaseCard from "./base-card";
 
 interface IProps {
-	cards: { id: string; text: string; worth?: string }[];
+	cards: { id: string; text: string; worth?: string | React.ReactNode }[];
 	showId?: boolean;
 	showWorth?: boolean;
+	square?: boolean;
 	className?: string;
 }
 
@@ -11,19 +12,13 @@ export default function CardsBlock({
 	cards,
 	showId,
 	showWorth,
+	square,
 	className,
 }: IProps) {
 	const getCards = () =>
-		cards.map((card) => (
-			<Card
-				key={card.id}
-				className={`card ${
-					showWorth &&
-					`flex flex-col items-center justify-center gap-[15px] aspect-square py-[15px]! px-[20px]! text-center`
-				}`}
-			>
-				{showId && <p>{card.id}.</p>}
-				{showWorth && (
+		cards.map((card) => {
+			const worth =
+				typeof card.worth === "string" ? (
 					<p
 						className={`font-bold ${
 							card.worth?.toLowerCase().includes("поддержка")
@@ -33,10 +28,24 @@ export default function CardsBlock({
 					>
 						{card.worth}
 					</p>
-				)}
-				<p className={`${showWorth && "text-[14px]"}`}>{card.text}</p>
-			</Card>
-		));
+				) : (
+					card.worth
+				);
+
+			return (
+				<BaseCard
+					key={card.id}
+					className={`card ${
+						showWorth &&
+						`flex flex-col items-center justify-center gap-[15px] min-h-[110px] py-[15px]! px-[20px]! text-center`
+					} ${square && "aspect-square"}`}
+				>
+					{showId && <p>{card.id}.</p>}
+					{showWorth && worth}
+					<p className={`${showWorth && "text-[14px]"}`}>{card.text}</p>
+				</BaseCard>
+			);
+		});
 
 	return (
 		<div className={`flex flex-col gap-[10px] ${className}`}>{getCards()}</div>

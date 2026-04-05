9d1e3affbd38c329ede23e58539a51c46a1bd5ca
Daniil Surin <wkingsurin@gmail.com>
2026-04-05 16:37:36 +0500

    feat(shared): create tag component, correct card paddings

--- components/shared/card.tsx
+++ components/shared/card.tsx
@@ -12,7 +12,7 @@ interface IProps {
 export default function Card({ className, options, children }: IProps) {
 	return (
 		<div
-			className={`flex items-center gap-[15px] px-[30px] py-3 min-h-[70px] bg-foreground rounded-[20px] border-transparent bg-linear-to-br ${options.borderWeight} ${options.fromColor} ${options.toColor} bg-clip-border [background-origin:border-box] ${options.shadowColor} ${className}`}
+			className={`flex items-center gap-3 px-[30px] py-3 min-h-[70px] bg-foreground rounded-[20px] border-transparent bg-linear-to-br ${options.borderWeight} ${options.fromColor} ${options.toColor} bg-clip-border [background-origin:border-box] ${options.shadowColor} ${className}`}
 		>
 			{children}
 		</div>
---
+++ components/shared/tag.tsx
@@ -0,0 +1,25 @@
+import Card from "./card";
+
+interface IProps {
+	className?: string;
+	children: React.ReactNode | string;
+	isPremium?: boolean;
+}
+
+export default function Tag({ className, children, isPremium }: IProps) {
+	const options = {
+		fromColor: "from-foreground-400/50",
+		toColor: "to-foreground/50",
+		borderWeight: "border-[0.5px]",
+		shadowColor: "[box-shadow:inset_0_0_0_100vw_theme(colors.foreground-300/30)]",
+	};
+
+	return (
+		<Card
+			className={`${className} px-3! py-[6px]! min-h-[30px]! bg-foreground-300! backdrop-blur p-[20px]! font-bold ${isPremium && 'text-accent'}`}
+			options={options}
+		>
+			{children}
+		</Card>
+	);
+}

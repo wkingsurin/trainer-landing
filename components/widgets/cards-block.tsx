360e79112fd01c521c5fdc14775a0458283dc74a
Daniil Surin <wkingsurin@gmail.com>
2026-04-05 10:31:58 +0500

    feat(widgets): improve CardsBlock widget
    - Added props: className and showWorth flag
    - Added p elements to display worth value
    - Extended UI styles
    - Extended types

--- components/widgets/cards-block.tsx
+++ components/widgets/cards-block.tsx
@@ -1,18 +1,44 @@
 import Card from "../shared/card";
 
 interface IProps {
-  cards: { id: string; text: string }[];
-  showId?: boolean;
+	cards: { id: string; text: string; worth?: string }[];
+	showId?: boolean;
+	showWorth?: boolean;
+	className?: string;
 }
 
-export default function CardsBlock({ cards, showId }: IProps) {
+export default function CardsBlock({
+	cards,
+	showId,
+	showWorth,
+	className,
+}: IProps) {
 	const getCards = () =>
 		cards.map((card) => (
-			<Card key={card.id}>
+			<Card
+				key={card.id}
+				className={`card ${
+					showWorth &&
+					`flex flex-col items-center justify-center gap-[15px] aspect-square py-[15px]! px-[20px]! text-center`
+				}`}
+			>
 				{showId && <p>{card.id}.</p>}
-				<p>{card.text}</p>
+				{showWorth && (
+					<p
+						className={`font-bold ${
+							card.worth?.toLowerCase().includes("поддержка")
+								? "text-[20px]"
+								: "text-[24px]"
+						}`}
+					>
+						{card.worth}
+					</p>
+				)}
+				<p className={`${showWorth && "text-[14px]"}`}>{card.text}</p>
 			</Card>
 		));
 
-	return <div className="flex flex-col gap-[10px]">{getCards()}</div>;
+	return (
+		<div className={`flex flex-col gap-[10px] ${className}`}>{getCards()}</div>
+	);
 }

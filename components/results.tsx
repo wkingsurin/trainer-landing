82631487c6e7367ab910e518eaab97a4dcfe0ff3
Daniil Surin <wkingsurin@gmail.com>
2026-04-06 14:45:13 +0500

    feat(components): create Results component

---
+++ components/results.tsx
@@ -0,0 +1,34 @@
+import Section from "./shared/section";
+import Carousel from "./widgets/carousel";
+import TitleBlock from "./widgets/title-block";
+
+export default function Results() {
+	const cards = [
+		{
+			title: "Карина",
+			description: "«Теперь я не боюсь ходить в зал в открытом топе»",
+		},
+		{
+			title: "Света",
+			description: "«Наконец-то я знаю, что делать»",
+		},
+		{
+			title: "Оксана",
+			description: "«Спасибо Алине за наставничество, благодаря ее навыку фитнес-тренера, я пришла к фигуре, которую хотела очень давно»",
+		},
+		{
+			title: "Екатерина",
+			description: "«Рекомендую тренера, однозначно. Пробовала тренироваться у других - результаты были минимальные, начала тренироваться с Алиной и результат на лицо»",
+		},
+	];
+
+	return (
+		<Section px>
+			<TitleBlock
+				subtitle="результаты"
+				title="Результаты моих клиентов говорят сами за себя"
+			/>
+			<Carousel slides={cards} />
+		</Section>
+	);
+}

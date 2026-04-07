7691b30ce22d3e9c9ccadb3f67db53eb139a747e
Daniil Surin <wkingsurin@gmail.com>
2026-04-07 12:19:44 +0500

    feat(root layout): render Footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({
 	variable: "--font-inter",
@@ -27,6 +28,7 @@ export default function RootLayout({
 			<body className="min-h-full flex flex-col text-surface">
 				<Header />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	);

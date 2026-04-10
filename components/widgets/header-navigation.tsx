ba0a24e2e7b2e7e4043c0fdc21ef45302d5f2501
Daniil Surin <wkingsurin@gmail.com>
2026-04-10 10:58:01 +0500

    feat(widgets): create HeaderNavigation component

---
+++ components/widgets/header-navigation.tsx
@@ -0,0 +1,31 @@
+import CTAButton from "../shared/cta-button";
+import MenuButton from "../shared/menu-button";
+import Navigation from "../shared/navigation";
+
+interface IProps {
+	isMobile: boolean;
+}
+
+export default function HeaderNavigation({ isMobile }: IProps) {
+	return (
+		<>
+			{!isMobile && (
+				<>
+					<Navigation />
+					<CTAButton>Записаться</CTAButton>
+				</>
+			)}
+
+			{isMobile && (
+				<div className="lg:hidden">
+					<MenuButton />
+				</div>
+			)}
+			
+			{/* <div className="hidden md:flex md:gap-[15px] lg:hidden">
+				<CTAButton>Записаться</CTAButton>
+				<MenuButton />
+			</div> */}
+		</>
+	);
+}

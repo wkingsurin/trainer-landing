7421d599af189d781e5cd38ef4a551a2bc34cb90
Daniil Surin <wkingsurin@gmail.com>
2026-04-10 15:02:00 +0500

    feat(main): Adapt Hero for mobile devices
    - Added condition styles for desktop or moblie devices
    - Changed image in BackgroundImage component

--- components/main/components/hero.tsx
+++ components/main/components/hero.tsx
@@ -1,22 +1,42 @@
+import isMobileDevice from "@/components/hooks/isDeviceType";
 import CTAButton from "../../shared/cta-button";
 import Section from "../../shared/section";
 import BackgroundImage from "../widgets/background-image";
+import SectionTitle from "@/components/shared/section-title";
 
-export default function Hero() {
+export default async function Hero() {
+	const isMobile = await isMobileDevice();
+	const titleBlockStyle = isMobile ? "bottom-[120px]" : "top-[250px] left-[120px] p-0! gap-6 items-center";
+	const sectionDesktopStyle = "container mx-auto max-w-[1280px] p-0!";
+	const textBlockSize = isMobile
+		? "max-w-[1280px]"
+		: "max-w-[480px] left-[120px]";
+	const titleStyle = isMobile ? "text-[32px]" : "text-[48px]";
+	const subtitleStyle = isMobile ? "text-base" : "text-[20px]"
+	const contentGap = isMobile ? "gap-[6px]" : "gap-3"
+	const buttonStyle = isMobile ? "min-h-[50px]" : "min-w-[240px] rounded-[16px] text-[18px] font-bold"
+
 	return (
-		<Section id="hero" className="relative flex flex-col h-[100vh] pt-0">
-			<BackgroundImage />
-			<div className="absolute bottom-[120px] px-3 z-1100 flex flex-col gap-[15px]">
-				<div className="flex flex-col gap-[6px] text-center">
-					<h1 className="font-bold text-[32px]">
+		<Section
+			id="hero"
+			className={`relative flex flex-col h-[100vh] pt-0 w-full box-content ${sectionDesktopStyle}`}
+		>
+			<BackgroundImage isMobile={isMobile} />
+			<div
+				className={`absolute px-3 z-1100 container mx-auto ${textBlockSize} flex flex-col gap-[15px] ${titleBlockStyle}`}
+			>
+				<div className={`flex flex-col gap-[6px] text-center ${contentGap}`}>
+					<SectionTitle className={`font-bold ${titleStyle}`}>
 						Фигура мечты — уже через 8 недель
-					</h1>
-					<span className="text-base">
+					</SectionTitle>
+					<span className={subtitleStyle}>
 						Персональные тренировки и питание под тебя — с поддержкой на каждом
 						этапе
 					</span>
 				</div>
-				<CTAButton>Начать</CTAButton>
+				<CTAButton isMobile={isMobile} className={buttonStyle}>
+					Начать
+				</CTAButton>
 			</div>
 		</Section>
 	);
--- components/main/widgets/background-image.tsx
+++ components/main/widgets/background-image.tsx
@@ -1,17 +1,27 @@
 import Image from "next/image";
 
-export default function BackgroundImage() {
+interface IProps {
+	isMobile?: boolean;
+}
+
+export default function BackgroundImage({ isMobile }: IProps) {
+	const imageStyle = isMobile ? "object-cover object-top top-[80px]!" : "object-contain object-right";
+
 	return (
 		<div className="absolute z-999 w-full h-full">
-			<div className="relative w-full h-full">
+			<div className="relative w-full h-full overflow-hidden">
+				{!isMobile && <>
+					<div className="absolute w-1/3 h-full bg-linear-to-r from-background to-background/0"></div>
+					<div></div>
+				</>}
 				<div className="absolute z-1200 w-full h-full bg-background-250"></div>
 				<div className="absolute z-1200 bottom-0 w-full h-100 bg-linear-to-b from-background/0 to-background"></div>
 				<Image
-					src="/4.jpg"
+					src="/hero.jpg"
 					alt="background"
 					fill
 					priority
-					className="object-cover object-top"
+					className={imageStyle}
 				/>
 			</div>
 		</div>

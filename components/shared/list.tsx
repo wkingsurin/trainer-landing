b001b578f3e3089f0bfd8edde0b66a2cba1438a5
Daniil Surin <wkingsurin@gmail.com>
2026-04-10 10:55:11 +0500

    feat(shared): improve List
    - Changed types
    - Adapted for deskto

--- components/shared/list.tsx
+++ components/shared/list.tsx
@@ -3,16 +3,18 @@ import SocialBlock from "../widgets/social-block";
 import ContentTitle from "./content-title";
 
 interface IProps {
-	title: string;
+	title?: string;
 	items: { id: string; text: string }[];
-	type: "contacts" | "info";
+	type: "contacts" | "info" | "navigation";
 }
 
 export default function List({ title, items, type }: IProps) {
+	const desktopStyle = "flex-row gap-[15px]";
+
 	return (
 		<div className="flex flex-col gap-3">
-			<ContentTitle className="text-surface-500">{title}</ContentTitle>
-			<ul className="flex flex-col gap-[8px]">
+			{title && <ContentTitle className="text-surface-500">{title}</ContentTitle>}
+			<ul className={`flex flex-col gap-[8px] ${type === 'navigation' && desktopStyle}`}>
 				{items.map((item) => (
 					<li key={item.id} className="">
 						<Link href={`#${item.id}`}>{item.text}</Link>

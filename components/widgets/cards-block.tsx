481f8760d98085a6d16ad5962b02acdf318e3c48
Daniil Surin <wkingsurin@gmail.com>
2026-04-10 16:44:48 +0500

    feat(widgets): improve CardsBlock widgets
    - Adapted CardsBlock for desktop
    - Added background image for odd cards

--- components/widgets/cards-block.tsx
+++ components/widgets/cards-block.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import BaseCard from "./base-card";
 
 interface IProps {
@@ -5,7 +6,9 @@ interface IProps {
 	showId?: boolean;
 	showWorth?: boolean;
 	square?: boolean;
+	isMobile?: boolean;
 	className?: string;
+	type: "advantage" | "problems" | "solution" | "services" | "about";
 }
 
 export default function CardsBlock({
@@ -13,8 +16,21 @@ export default function CardsBlock({
 	showId,
 	showWorth,
 	square,
+	isMobile,
 	className,
+	type,
 }: IProps) {
+	const blockStyle = isMobile ? "flex-col" : "gap-5 flex-row z-1200 w-full";
+	const cardStyle = isMobile ? "min-h-[70px]" : "min-h-[120px] font-bold";
+	const numberStyle = isMobile
+		? "text-[36px] text-accent"
+		: "text-[36px] text-accent font-semibold";
+	const imageStyle = isMobile
+		? "object-cover object-[center_-30px]"
+		: "object-cover object-top";
+
+	const idIsOdd = (id: string) => Number(id) % 2 !== 0;
+
 	const getCards = () =>
 		cards.map((card) => {
 			const worth =
@@ -35,19 +51,29 @@ export default function CardsBlock({
 			return (
 				<BaseCard
 					key={card.id}
-					className={`card ${
+					className={`card relative group overflow-hidden ${
 						showWorth &&
 						`flex flex-col items-center justify-center gap-[15px] min-h-[110px] py-[15px]! px-[20px]! text-center`
-					} ${square && "aspect-square" || 'flex-1'}`}
+					} ${(square && "aspect-square") || "flex-1"} ${cardStyle}`}
 				>
-					{showId && <p>{card.id}.</p>}
+					{showId && <p className={numberStyle}>{card.id}</p>}
 					{showWorth && worth}
 					<p className={`${showWorth && "text-[14px]"}`}>{card.text}</p>
+					{type === "problems" && idIsOdd(card.id) && (
+						<Image
+							src={`/pain-${card.id}.jpg`}
+							alt="Girl"
+							fill
+							className={`absolute ${imageStyle} rounded-[16px] opacity-0 scale-105 transition duration-[0.7s] grayscale group-hover:opacity-20 group-hover:scale-100`}
+						/>
+					)}
 				</BaseCard>
 			);
 		});
 
 	return (
-		<div className={`flex flex-col gap-[10px] ${className}`}>{getCards()}</div>
+		<div className={`flex gap-[10px] ${blockStyle} ${className}`}>
+			{getCards()}
+		</div>
 	);
 }

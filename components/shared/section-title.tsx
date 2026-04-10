41988252595da4a047235ce9a47b47f2fc2a22c5
Daniil Surin <wkingsurin@gmail.com>
2026-04-10 15:00:48 +0500

    feat(shared): add className prop in SectionTitle

--- components/shared/section-title.tsx
+++ components/shared/section-title.tsx
@@ -1,7 +1,12 @@
 interface IProps {
+	className?: string;
 	children: React.ReactNode | string;
 }
 
-export default function SectionTitle({ children }: IProps) {
-	return <h2 className="text-[20px] text-center font-bold">{children}</h2>;
+export default function SectionTitle({ className, children }: IProps) {
+	return (
+		<h2 className={`text-[20px] text-center font-bold ${className}`}>
+			{children}
+		</h2>
+	);
 }

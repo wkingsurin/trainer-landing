dbfb161f6d1885dff5b259eb4da2a038301a65d9
Daniil Surin <wkingsurin@gmail.com>
2026-04-10 11:02:14 +0500

    feat(components): adapt Header for desktop

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,14 @@
-import MenuButton from "./shared/menu-button";
+import isMobileDevice from "./hooks/isDeviceType";
+import HeaderNavigation from "./widgets/header-navigation";
 
-export default function Header() {
+export default async function Header() {
+	const isMobile = await isMobileDevice()
+
 	return (
 		<header className="fixed z-1999 flex w-full min-h-[80px] bg-foreground-700 backdrop-blur-2xl border-b-[0.5] border-foreground-500 shadow-sh">
-			<div className="container mx-auto px-3 flex items-center justify-between min-h-[80px]">
+			<div className="container mx-auto max-w-[1280px] px-3 flex items-center justify-between min-h-[80px]">
         <span className="flex flex-wrap w-min text-accent font-bold text-[18px]">Алина Самойлова</span>
-				<MenuButton />
+				<HeaderNavigation isMobile={isMobile} />
 			</div>
 		</header>
 	);

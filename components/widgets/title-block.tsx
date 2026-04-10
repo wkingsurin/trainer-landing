4d5370af65780c0cd88a0864fb0a35993d2e3ac9
Daniil Surin <wkingsurin@gmail.com>
2026-04-10 16:44:22 +0500

    feat(widgets): adapt TitleBlock widget for desktop

--- components/widgets/title-block.tsx
+++ components/widgets/title-block.tsx
@@ -4,13 +4,22 @@ import SectionTitle from "../shared/section-title";
 interface IProps {
 	title: string;
 	subtitle: string;
+	className?: string;
+	isMobile?: boolean;
 }
 
-export default function TitleBlock({ subtitle, title }: IProps) {
+export default function TitleBlock({
+	className,
+	isMobile = true,
+	subtitle,
+	title,
+}: IProps) {
+	const style = isMobile ? "gap-[10px]" : "max-w-[420px] box-content gap-5";
+
 	return (
-		<div className="flex flex-col gap-[10px] px-3">
-			<Subtitle>{subtitle}</Subtitle>
-			<SectionTitle>{title}</SectionTitle>
+		<div className={`flex flex-col px-3 ${style} ${className}`}>
+			<Subtitle isMobile={isMobile}>{subtitle}</Subtitle>
+			<SectionTitle isMobile={isMobile}>{title}</SectionTitle>
 		</div>
 	);
 }

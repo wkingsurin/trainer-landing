b81b83562654fa9e10aab3cdb2576a3dc7f77705
Daniil Surin <wkingsurin@gmail.com>
2026-04-11 14:27:02 +0500

    feat(shared): improve CTAButton and SectionContent components
    - Added function to define size of CTAButton
    - Added type flag for CTAButton
    - Added className prop for SectionContent

--- components/shared/cta-button.tsx
+++ components/shared/cta-button.tsx
@@ -4,13 +4,44 @@ interface IProps {
 	className?: string;
 	isMobile?: boolean;
 	children: React.ReactNode | string;
+	type: "normal" | "medium" | "huge";
 }
 
-export default function CTAButton({ className, isMobile, children }: IProps) {
-	const size = isMobile ? "w-full min-w-[50px] h-[50px]" : 'w-auto min-w-[180px] min-h-[50px]'
+export default function CTAButton({
+	className,
+	isMobile,
+	type,
+	children,
+}: IProps) {
+	function getSize() {
+		let size = "w-full min-w-[50px] h-[50px]";
+
+		switch (type) {
+			case "normal": {
+				if (!isMobile) {
+					size = "w-auto min-w-[180px] min-h-[50px]";
+					return size;
+				}
+			}
+			case "medium": {
+				if (!isMobile) {
+					size = "w-auto min-w-[320px] min-h-[50px]";
+					return size;
+				}
+			}
+			case "huge": {
+				if (!isMobile) {
+					size = "w-auto min-w-[320px] min-h-[50px]";
+					return size;
+				}
+			}
+		}
+	}
 
 	return (
-		<Button className={`flex items-center justify-center bg-accent ${size} py-[10px] border-[0.5px] border-transparent bg-linear-to-br from-surface-400/25 to-surface/25 bg-clip-border [background-origin:border-box] [box-shadow:inset_0_0_0_100vw_theme(colors.accent)] font-medium rounded-[12px] ${className}`}>
+		<Button
+			className={`flex items-center justify-center bg-accent ${getSize()} py-[10px] border-[0.5px] border-transparent bg-linear-to-br from-surface-400/25 to-surface/25 bg-clip-border [background-origin:border-box] [box-shadow:inset_0_0_0_100vw_theme(colors.accent)] font-medium rounded-[12px] ${className}`}
+		>
 			{children}
 		</Button>
 	);
--- components/shared/section-content.tsx
+++ components/shared/section-content.tsx
@@ -1,10 +1,12 @@
 interface IProps {
+	className?: string;
 	gap?: string;
 	children: React.ReactNode;
 }
 
-export default function SectionContent({ gap, children }: IProps) {
+export default function SectionContent({ className, gap, children }: IProps) {
+	const style = className ? className : ''
 	const gapClass = (gap && `gap-[${gap}px]`) || "";
 
-	return <div className={`flex flex-col ${gapClass}`}>{children}</div>;
+	return <div className={`flex flex-col ${gapClass} ${style}`}>{children}</div>;
 }

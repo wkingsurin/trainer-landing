907ce9aea1012810e608afd4b3a5d5bebd5605d9
Daniil Surin <wkingsurin@gmail.com>
2026-04-13 18:06:12 +0500

    style(main/widgets): fix BackgroundImage top position and add linear-gradient overlays

--- components/main/widgets/background-image.tsx
+++ components/main/widgets/background-image.tsx
@@ -8,17 +8,17 @@ export default function BackgroundImage({ isMobile }: IProps) {
 	const imageStyle = isMobile
 		? "object-cover object-top"
 		: "object-contain object-right";
-	const layerStyle = isMobile ? "top-[80px]" : "top-[120px]";
+	const layerStyle = isMobile ? "top-[80px]" : "top-[100px]";
 
 	return (
-		<div className={`absolute z-999 w-full h-full ${layerStyle}`}>
+		<div className={`absolute right-[0px] z-999 max-w-[720px] w-full h-full ${layerStyle}`}>
 			<div className="relative w-full h-full overflow-hidden">
 				{!isMobile && (
 					<>
-						<div className="absolute w-1/3 h-full bg-linear-to-r from-background to-background/0"></div>
+						<div className="absolute z-1200 w-1/3 h-full bg-linear-to-r from-background to-background/0"></div>
+						<div className="absolute z-1200 right-0 w-1/3 h-full bg-linear-to-r from-background/0 to-background"></div>
 					</>
 				)}
-				<div className="absolute z-1200 w-full h-full bg-background-250"></div>
 				<div className="absolute z-1200 bottom-0 w-full h-100 bg-linear-to-b from-background/0 to-background"></div>
 				<Image
 					src="/hero.jpg"

f572753d0d4d72c1faa507250db8b81258aad641
Daniil Surin <wkingsurin@gmail.com>
2026-04-18 16:34:50 +0500

    fix: remove body jump, add scrollbar stable
    - Fixed LoadingScreen animation's logic
    - Lifted up motion.div from Screen to LoadingScreen

--- components/widgets/loading-screen/loading-screen.tsx
+++ components/widgets/loading-screen/loading-screen.tsx
@@ -1,34 +1,62 @@
 "use client";
 
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import Screen from "./screen";
 import { useUIStore } from "@/lib/store/ui.store";
 import { useEffect } from "react";
 import { setCookie } from "@/app/cookie-action";
 
 export default function LoadingScreen() {
+	const appReady = useUIStore((s) => s.appReady);
 	const setAppReady = useUIStore((s) => s.setAppReady);
 
 	useEffect(() => {
-		document.body.style.overflowY = "hidden";
-
-		const scrollTimer = setTimeout(() => {
-			document.body.style.overflowY = "auto";
-			setAppReady(true);
-		}, 3000);
 		const timer = setTimeout(async () => {
+			setAppReady(true);
 			setCookie();
 		}, 3500);
 
 		return () => {
 			clearTimeout(timer);
-			clearTimeout(scrollTimer);
 		};
 	}, [setAppReady]);
 
+	useEffect(() => {
+		if (appReady) {
+			const unlockTimer = setTimeout(() => {
+				document.body.style.overflow = "auto";
+			}, 600);
+
+			return () => clearTimeout(unlockTimer);
+		}
+	}, [appReady]);
+
 	return (
 		<AnimatePresence mode="wait">
-			<Screen key="loader" />
+			{!appReady && (
+				<motion.div
+					style={{
+						position: "fixed",
+						top: 0,
+						left: 0,
+						width: "100%",
+						height: "100dvh",
+						display: "flex",
+						justifyContent: "center",
+						background: "oklch(0.1708 0.0103 285.25)",
+						alignItems: "center",
+						zIndex: 2100,
+					}}
+					key="loader"
+					initial={{ opacity: 1 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{ duration: 0.5, ease: "easeInOut" }}
+					className="loading-screen"
+				>
+					<Screen key="loader" />
+				</motion.div>
+			)}
 		</AnimatePresence>
 	);
 }

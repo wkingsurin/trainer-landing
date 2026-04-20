c7cec228e609232147b81b43d5fefba72e04322b
Daniil Surin <wkingsurin@gmail.com>
2026-04-20 15:01:52 +0500

    perf(backgroundImage): improve LCP image load speed

--- components/main/widgets/background-image.tsx
+++ components/main/widgets/background-image.tsx
@@ -2,22 +2,25 @@
 
 import Image from "next/image";
 import image from "@/public/hero.jpg";
+import { memo } from "react";
 
 interface IProps {
 	isMobile?: boolean;
 }
 
-export default function BackgroundImage({ isMobile }: IProps) {
+export default memo(BackgroundImage)
+
+function BackgroundImage({ isMobile }: IProps) {
 	const imageStyle = isMobile
-		? "object-cover object-top"
+		? "object-cover object-top object-left w-[100%] h-[100%]"
 		: "object-contain object-right";
-	const layerStyle = isMobile ? "top-[80px]" : "top-[100px]";
+	const layerStyle = isMobile ? "top-[80px]" : "top-[100px] aspect-[9/16]";
 
 	return (
 		<div
-			className={`absolute right-[0px] z-999 max-w-[720px] w-full h-full ${layerStyle}`}
+			className={`absolute right-[0px] z-999 max-w-[720px] ${layerStyle}`}
 		>
-			<div className="relative w-full h-full overflow-hidden">
+			<div className="relative w-full aspect-[12/16] overflow-hidden">
 				{!isMobile && (
 					<>
 						<div className="absolute inset-y-0 left-0 z-[1200] w-1/3 bg-gradient-to-l from-transparent via-background/60 to-background" />
@@ -32,9 +35,15 @@ export default function BackgroundImage({ isMobile }: IProps) {
 				<Image
 					src={image}
 					alt="background"
-					preload
-					className={`transition duration-500 ease-in-out opacity-0 ${imageStyle}`}
-					onLoadingComplete={(img) => img.classList.remove("opacity-0")}
+					priority
+					fetchPriority="high"
+					sizes="(max-width: 640px) 100vw, (max-width: 1200px) 50vw, 33vw"
+					quality={isMobile ? 75 : 90}
+					loading="eager"
+					className={`transition duration-500 ease-in-out max-w-[720px] ${imageStyle}`}
+					onLoad={(img) =>
+						img.currentTarget.classList.replace("opacity-[0.01]", "opacity-100")
+					}
 				/>
 			</div>
 		</div>

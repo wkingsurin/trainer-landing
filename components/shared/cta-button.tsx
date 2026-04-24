6109d4ba7b4e960bf0ab718051f2d4a27256bd0a
Daniil Surin <wkingsurin@gmail.com>
2026-04-24 16:44:18 +0500

    feat(CTAButton): add form flag to return submit button

--- components/shared/cta-button.tsx
+++ components/shared/cta-button.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useUIStore } from "@/lib/store/ui.store";
 import { Button } from "../ui/button";
 
 interface IProps {
@@ -5,6 +8,7 @@ interface IProps {
 	isMobile?: boolean;
 	children: React.ReactNode | string;
 	type: "normal" | "medium" | "huge";
+	form?: boolean;
 }
 
 export default function CTAButton({
@@ -12,7 +16,12 @@ export default function CTAButton({
 	isMobile,
 	type,
 	children,
+	form,
 }: IProps) {
+	const onClick = () => {
+		useUIStore.getState().setModalOpen(true);
+	};
+
 	function getSize() {
 		let size = "w-full min-w-[50px] h-[50px]";
 
@@ -38,11 +47,25 @@ export default function CTAButton({
 		}
 	}
 
+	if (form) {
+		return (
+			<Button
+				className={`flex items-center justify-center bg-accent ${
+					isMobile ? "w-full min-h-[60px]" : getSize()
+				} py-[10px] border-[0.5px] border-transparent bg-linear-to-br from-surface-400/25 to-surface/25 bg-clip-border [background-origin:border-box] [box-shadow:inset_0_0_0_100vw_theme(colors.accent)] text-[16px] font-medium rounded-[16px] tracking-[2%] ${className}`}
+				type="submit"
+			>
+				{children}
+			</Button>
+		);
+	}
+
 	return (
 		<Button
 			className={`flex items-center justify-center bg-accent ${
 				isMobile ? "w-full min-h-[60px]" : getSize()
 			} py-[10px] border-[0.5px] border-transparent bg-linear-to-br from-surface-400/25 to-surface/25 bg-clip-border [background-origin:border-box] [box-shadow:inset_0_0_0_100vw_theme(colors.accent)] text-[16px] font-medium rounded-[16px] tracking-[2%] ${className}`}
+			onClick={onClick}
 		>
 			{children}
 		</Button>
